94bd29adfd6377be1f5b991bb99877f18df3da2b
wata00913 <175d8639@gmail.com>
2022-11-07 05:53:42 +0900

    Memoクラスを作成

---
+++ 04.class/memo.js
@@ -0,0 +1,56 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+class Memo {
+  constructor({ id, content = "" }) {
+    this._id = id ?? crypto.randomUUID();
+    this._content = content;
+  }
+
+  create() {
+    const all = Memo.all();
+    all.push(this);
+
+    fs.writeFileSync(Memo.PATH, JSON.stringify({ all: all }));
+  }
+
+  find(id) {
+    const all = Memo.all();
+    return all.find((data) => data === id);
+  }
+
+  destroy() {
+    const all = Memo.all();
+    const targetIdx = all.findIndex((data) => data === this.id);
+
+    if (targetIdx === -1)
+      throw "Can not delete because it is new or has been deleted.";
+
+    all.splice(targetIdx, 1);
+    fs.writeFileSync(Memo.PATH, JSON.stringify({ all: all }));
+  }
+
+  static all() {
+    const str = fs.readFileSync(this.PATH);
+    const allData = JSON.parse(str).all;
+    return allData.map((data) => {
+      return new this({ id: data._id, content: data._content });
+    });
+  }
+
+  static get PATH() {
+    return "./data.json";
+  }
+
+  get content() {
+    return this._content;
+  }
+
+  set content(val) {
+    this._content = val;
+  }
+
+  get id() {
+    return this._id;
+  }
+}
